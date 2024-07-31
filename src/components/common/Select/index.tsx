4bde09517fe7695baa44a49f05d591c3fb4990bd
Andreushk <andrewshkt@gmail.com>
2024-07-31 12:12:40 +0300

    bugsix: openning select on its open button click

--- src/components/common/Select/index.tsx
+++ src/components/common/Select/index.tsx
@@ -8,41 +8,28 @@ import styles from './styles.module.scss';
 interface ComponentProps {
   name: string;
   options: string[];
-  error?: string | null;
+  error: string | null;
   disabled: boolean;
-  register?: UseFormRegister<any>;
+  register: UseFormRegister<any>;
 }
 
-const Select: React.FC<ComponentProps> = ({ name, options, error, disabled, register }) => {
-  const selectOptions = options.map((value) => (
-    <option key={value} value={value}>
-      {value}
-    </option>
-  ));
-
-  return register ? (
-    <div className={styles.wrapper}>
-      <div className={styles.wrapper_arrow}>
-        <ArrowDown />
-      </div>
-      <select
-        {...register(name)}
-        disabled={disabled}
-        className={`${inter.className} ${styles.select} ${error ? styles.error : ''}`}
-      >
-        {selectOptions}
-      </select>
+const Select: React.FC<ComponentProps> = ({ name, options, error, disabled, register }) => (
+  <div className={styles.wrapper}>
+    <div className={styles.wrapper_arrow}>
+      <ArrowDown />
     </div>
-  ) : (
-    <div className={styles.wrapper}>
-      <div className={styles.wrapper_arrow}>
-        <ArrowDown />
-      </div>
-      <select disabled={disabled} className={`${inter.className} ${styles.select}`}>
-        {selectOptions}
-      </select>
-    </div>
-  );
-};
+    <select
+      {...register(name)}
+      disabled={disabled}
+      className={`${inter.className} ${styles.select} ${error ? styles.error : ''}`}
+    >
+      {options.map((value) => (
+        <option key={value} value={value}>
+          {value}
+        </option>
+      ))}
+    </select>
+  </div>
+);
 
 export default Select;
