f3ef5c910e8d18d8214846d926777c978c970ee5
Andreushk <andrewshkt@gmail.com>
2024-07-31 11:40:02 +0300

    feature: better language select

--- src/components/common/LanguageSelect/index.tsx
+++ src/components/common/LanguageSelect/index.tsx
@@ -1,30 +1,37 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useLocale } from 'next-intl';
-import React, { useTransition } from 'react';
+import React from 'react';
+
+import { sen } from '@/styles/fonts';
 
 import styles from './style.module.scss';
 
 const LanguageSelect: React.FC = () => {
-  const [, startTransition] = useTransition();
-  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
   const localActive = useLocale();
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     const nextLocale = e.target.value;
-    startTransition(() => {
-      router.replace(`/${nextLocale}`);
-    });
+
+    const params = new URLSearchParams(searchParams);
+    const pathnameParts = pathname.split('/');
+    pathnameParts[1] = nextLocale;
+    const newPath = pathnameParts.join('/');
+
+    replace(`${newPath}${params.size > 0 ? `?${params}` : ''}`, { scroll: false });
   };
 
   return (
-    <label htmlFor="lang-select" className={styles.select}>
-      <select defaultValue={localActive} id="lang-select" onChange={handleSelectChange}>
+    <div className={styles.select}>
+      <select className={sen.className} defaultValue={localActive} onChange={handleSelectChange}>
         <option value="en">English</option>
         <option value="ru">Russian</option>
       </select>
-    </label>
+    </div>
   );
 };
 
