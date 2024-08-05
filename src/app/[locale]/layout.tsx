f0119c4425266105212aa93f9d22c3368d28f5a5
Andreushk <andrewshkt@gmail.com>
2024-08-05 10:44:00 +0300

    bugfix: inter font and default letter spacing for Russian language

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,10 @@
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider, useLocale, useMessages } from 'next-intl';
 
 import { Footer, Header } from '@/components';
-import { sen } from '@/styles/fonts';
+import { inter, sen } from '@/styles/fonts';
+import { LocaleTypes } from '@/types/locale';
+
+import styles from './styles.module.scss';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,9 +13,13 @@ interface LayoutProps {
 
 const RootLayout: React.FC<LayoutProps> = ({ children, params }) => {
   const messages = useMessages();
+  const locale = useLocale() as LocaleTypes;
+
   return (
     <html lang={params.locale}>
-      <body className={sen.className}>
+      <body
+        className={`${locale === 'ru' ? inter.className : sen.className} ${locale === 'ru' ? styles.default_letter_spacing : ''}`}
+      >
         <NextIntlClientProvider messages={messages}>
           <Header />
           <main>{children}</main>
